08725f1cf174272a194530768cb21f4bf293d33a
Admin <Admin@DESKTOP-4H2CDN2>
2019-02-20 09:27:43 -0500

    init git

---
+++ src/selenium/Topic_01_CheckEnvironment.java
@@ -0,0 +1,38 @@
+package selenium;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Topic_01_CheckEnvironment {
+	WebDriver driver;
+  @Test
+  public void TC_01_CheckUrl() {
+	  String homePageUrl = driver.getCurrentUrl();
+	  Assert.assertEquals(homePageUrl, "http://live.guru99.com/");
+  }
+  
+  @Test
+  public void TC_02_CheckTitle(){
+	  String homePageTitle = driver.getTitle();
+	  Assert.assertEquals(homePageTitle, driver.getTitle());
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  driver.manage().window().maximize();
+	  driver.get("http://live.guru99.com/");
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  driver.quit();
+  }
+
+}
